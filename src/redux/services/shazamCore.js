0c5f6d48585d5ab58d86e7c10f1168f058bb7926
FrancisJTR7 <xgalasko@outlook.com>
2023-06-11 22:41:58 -0400

    APPLICATION FINISHED

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -17,11 +17,18 @@ export const shazamCoreApi = createApi({
     getSongDetails: builder.query({
       query: ({ songid }) => `/v1/tracks/details?track_id=${songid}`,
     }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `v1/charts/genre-world?genre_code=${genre}`,
+    }),
     getSongRelated: builder.query({
       query: ({ songid }) => `/v1/tracks/related?track_id=${songid}`,
     }),
     getArtistDetails: builder.query({
       query: (artistid) => `/v2/artists/details?artist_id=${artistid}`,
+    }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
     }),
     getSongsByCountry: builder.query({
       query: (countryCode) => `v1/charts/country?country_code=${countryCode}`,
@@ -35,4 +42,6 @@ export const {
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
   useGetSongsByCountryQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsBySearchQuery,
 } = shazamCoreApi;
